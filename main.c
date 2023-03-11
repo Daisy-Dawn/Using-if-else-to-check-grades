a75ce7306a94ad791119f958242fb2fcdb80c5ca
Agbo Amarachukwu Emmanuella <agboella20@gmail.com>
2023-03-11 11:53:30 +0000

    I modified it to include excess mark as wrong input

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello World\n");
+//Write a program that checks the grades of students based on the mark entered by user
+
+int main(void) 
+{
+  int grade;
+
+  printf("Please enter your mark:\t");
+  scanf("%d", &grade);
+
+  if (grade > 100)
+    printf("Wrong input\n");
+
+  else if (grade >= 80)
+    printf("Your grade is A\n");
+
+  else if (grade >= 70)
+    printf("Your grade is B\n");
+
+  else if (grade >= 60)
+    printf("Your grade is C\n");
+
+  else if (grade >= 50)
+    printf("Your grade is D\n");
+
+  else
+    printf("Your grade is E, please do better\n");
+
+  printf("\nEnd of program\n");
+  sleep(5);
+  system("clear");
+  printf("Welcome User");
+  
   return 0;
 }
